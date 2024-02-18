d5c1215988bc2e749f16c6492bde139b189acd1c
MidasNG <kyrdia2@alu.edu.gva.es>
2024-02-18 11:30:15 +0100

    Script de diálogos

---
+++ Assets/Scripts/Dialogue.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Dialogue : Interactive
+{
+    private Coroutine speaking;
+    private int textBoxAmount, currentBox, newBox;
+
+    [SerializeField] private string textShown;
+    [SerializeField] private TextMeshProUGUI target;
+    [SerializeField] private List<string> textList;
+
+    void Start()
+    {
+        textBoxAmount = textList.Count;
+    }
+
+    public override void Interact()
+    {
+        if (speaking == null) speaking = StartCoroutine(Speech());
+        else if (newBox < textBoxAmount-1) newBox++;
+    }
+
+    private IEnumerator Speech()
+    {
+        for (int i = 0; i < textList.Count;)
+        {
+            currentBox = newBox;
+            for (int j = 0; j < textList[currentBox].ToCharArray().Length; j++)
+            {
+                if (newBox > currentBox) break;
+                textShown = textShown + textList[currentBox].ToCharArray()[j].ToString();
+                if (target != null) target.text = textShown;
+                yield return new WaitForSeconds(.25f);
+            }
+            Debug.Log("Esperando interacción");
+            yield return new WaitUntil(() => newBox > currentBox);
+            textShown = null;
+        }
+    }
+}
